b988e19a857366e932a8d898c8c6370cae0acf48
mateopolci <mateopolci@hotmail.com>
2024-09-13 22:04:44 -0300

    .

--- tp2/webapi.go
+++ tp2/webapi.go
@@ -1,1 +1,16 @@
 package main
+
+import(
+	"github.com/gin-gonic/gin"
+	"github.com/jinzhu/gorm"
+	"github.com/lib/pq"
+)
+
+func init(){
+	var err error
+	db, err = gorm.Open("postgres", "host=localhost user=")
+}
+
+func main(){
+
+}
